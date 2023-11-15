84db31290984a153f6b053552f5485e11879c2a2
BackrndSource <back.rndsource@gmail.com>
2023-11-16 00:26:50 +0100

    Add order_by to templatetags

--- app/seosite/shop/templatetags/shop_extras.py
+++ app/seosite/shop/templatetags/shop_extras.py
@@ -2,32 +2,35 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def sub(value, value2):
-    return float(value)-float(value2)
+    return float(value) - float(value2)
+
 
 @register.filter
 def price_decimals(value):
     try:
-        result = '{:0>2.0f}'.format(float(value)%1*100)
+        result = "{:0>2.0f}".format(float(value) % 1 * 100)
     except:
         result = "00"
-    return result 
+    return result
 
 
 @register.filter
 def price_int(value):
     try:
-        result = int(float(value)//1)
+        result = int(float(value) // 1)
     except:
         result = ""
     return result
 
+
 @register.filter
 def childs(category):
-    return category.childs.filter(visible=True)
+    return category.childs.filter(visible=True).order_by("featured")
+
 
 @register.filter
 def products(category):
-    return category.products.filter(visible=True)
-
+    return category.products.filter(visible=True).order_by("featured", "-rating_count", "-last_modified")
